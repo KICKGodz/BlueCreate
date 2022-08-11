cab9f776ae7f598895ec9fdfc729d04fc72aa212
Media <719coolboy@gmai.com>
2022-08-10 22:10:14 -0400

    Added Orbit Controls

--- dist/content/web/init.js
+++ dist/content/web/init.js
@@ -8,40 +8,44 @@ const camera = new THREE.PerspectiveCamera(
 	0.1,
 	1000
 );
+camera.position.set( 0, 5, 5 );
+camera.lookAt( 0, 0, 0 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var model = MCModel.MinecraftModel.fromJson({
-	__comment:
-		"Model generated using MrCrayfish's Model Creator (http://mrcrayfish.com/modelcreator/)",
-	parent: 'block/block',
-	textures: {
-		particle: 'create:block/axis',
-		0: 'create:block/axis',
-		1: 'create:block/axis_top',
-	},
-	elements: [
-		{
-			name: 'Axis',
-			from: [6.0, 0.0, 6.0],
-			to: [10.0, 16.0, 10.0],
-			faces: {
-				north: { texture: '#0', uv: [6.0, 0.0, 10.0, 16.0] },
-				east: { texture: '#0', uv: [6.0, 0.0, 10.0, 16.0] },
-				south: { texture: '#0', uv: [6.0, 0.0, 10.0, 16.0] },
-				west: { texture: '#0', uv: [6.0, 0.0, 10.0, 16.0] },
-				up: { texture: '#1', uv: [6.0, 6.0, 10.0, 10.0] },
-				down: { texture: '#1', uv: [6.0, 6.0, 10.0, 10.0] },
-			},
-		},
-	],
-});
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
 
-//add group
-const group = new THREE.Group();
-group.add(model.elements);
+// var model = MCModel.MinecraftModel.fromJson({
+// 	__comment:
+// 		"Model generated using MrCrayfish's Model Creator (http://mrcrayfish.com/modelcreator/)",
+// 	parent: 'block/block',
+// 	textures: {
+// 		particle: 'create:block/axis',
+// 		0: 'create:block/axis',
+// 		1: 'create:block/axis_top',
+// 	},
+// 	elements: [
+// 		{
+// 			name: 'Axis',
+// 			from: [6.0, 0.0, 6.0],
+// 			to: [10.0, 16.0, 10.0],
+// 			faces: {
+// 				north: { texture: '#0', uv: [6.0, 0.0, 10.0, 16.0] },
+// 				east: { texture: '#0', uv: [6.0, 0.0, 10.0, 16.0] },
+// 				south: { texture: '#0', uv: [6.0, 0.0, 10.0, 16.0] },
+// 				west: { texture: '#0', uv: [6.0, 0.0, 10.0, 16.0] },
+// 				up: { texture: '#1', uv: [6.0, 6.0, 10.0, 10.0] },
+// 				down: { texture: '#1', uv: [6.0, 6.0, 10.0, 10.0] },
+// 			},
+// 		},
+// 	],
+// });
+
+// //add group
+// const group = new THREE.Group();
+// group.add(model.elements);
 
 const geometry = new THREE.BoxGeometry(3, 1, 3);
 const material = new THREE.MeshBasicMaterial({
@@ -53,7 +57,7 @@ scene.add(cube);
 
 function animate() {
 	requestAnimationFrame(animate);
-	cube.rotation.y += 0.005;
+     controls.update();
 	renderer.render(scene, camera);
 }
 
