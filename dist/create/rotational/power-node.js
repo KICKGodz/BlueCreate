026b93db598fe202a7a43ed53d62033f4a68a2a8
Ryder Cederberg <ryder@lform.com>
2022-08-29 17:22:02 -0400

    Added Power Nodes

---
+++ dist/create/rotational/power-node.js
@@ -0,0 +1,9 @@
+// Gearboxes, Shafts
+class PowerNode {
+	constructor(pos, sides, speed, child = null) {
+		this.pos = pos;
+		this.sides = sides;
+		this.speed = speed;
+		this.child = child;
+	}
+}
